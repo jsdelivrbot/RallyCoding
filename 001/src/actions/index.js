bf23eea9491d625e56cf18f5ab4b93e2607dd3ed
MiguelRamBalt <miguel.ramirez.baltazar@gmail.com>
2017-06-13 09:29:10 -0500

    Rally

---
+++ 001/src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+export function fetchUsers() {
+  const request = axios.get('http://jsonplaceholder.typicode.com/users');
+
+  return (dispatch)=>{
+    request.then( ({data})=>{
+      dispatch(
+        {type: 'FETCH_PROFILE' ,
+        payload : data});
+    });
+  };
+}
