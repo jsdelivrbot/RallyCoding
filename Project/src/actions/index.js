d00c34eecc563e2de0859d28f10fe65903f4c30f
MiguelRamBalt <miguel.ramirez.baltazar@gmail.com>
2017-06-17 11:20:20 -0500

    }

---
+++ Project/src/actions/index.js
@@ -0,0 +1,15 @@
+//if in the future is implemented a Rest Service where is retreaving
+//all information as a JSon we are going to create a Axios request.
+
+export function () => {
+  return{
+  usa: {
+        bills:['1','2','5','10','20','50','100','500','1000','5000','10000'],
+        coins:['0.01','0.05','0.1','0.25','0.5','1']
+      }
+  mex:{
+        bills:['20','50','100','200','500','1000'],
+        coins:['0.05','0.1','0.5','1','2','5','10','20']
+      }
+  }
+}
