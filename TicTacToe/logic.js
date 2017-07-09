80fb9dfc722edd122b6f88dcf5b39cb995089c0b
MiguelRamBalt <miguel.ramirez.baltazar@gmail.com>
2017-07-09 00:12:14 -0500

    Adding Game Logic and Events

--- TicTacToe/logic.js
+++ TicTacToe/logic.js
@@ -1,34 +1,51 @@
 function initialGame() {
   this.elements= {
-    l1:'',
-    l2:'',
-    l3:'',
-    l4:'',
-    l5:'',
-    l6:'',
-    l7:'',
-    l8:'',
-    l9:''
-  }
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+    7:'',
+    8:'',
+    9:''
+  };
+  this.whoPlays='X';
 }
 class gameFlow extends initialGame {
   constructor() {
     super();
   }
-}
-function elementSelected() {
-  console.log(this.id);
+  checkWhoIsTheWinner(){
+    if (this.elements[1] === this.elements[2] && this.elements[1]=== this.elements[3] &&this.elements[1]!== '') {
+      return true;
+    } else {
+
+    }
+    return false;
+  }
 }
 
-function addClickEventToElements(...spaceElements) {
+const newGame = new gameFlow()
+let elements = newGame.elements;
+
+function elementSelected () {
+ this.innerText = this.newGame.whoPlays;
+ this.newGame.elements[this.id]=this.newGame.whoPlays;
+ this.newGame.whoPlays= this.newGame.whoPlays === 'X'? 'O' : 'X';
+ console.log(this.newGame.checkWhoIsTheWinner());
+}(0,this.newGame);
+
+function addClickEventToElements(newGame,...spaceElements) {
   return spaceElements.map((element)=>{
+    element.newGame= newGame;
     return element.addEventListener('click', elementSelected);
   });
 }
 
-let elements = new gameFlow().elements;
-//console.log(elements);
 
 const spaceElements = document.querySelectorAll('.space') || [];
-
-const elementsAdded =addClickEventToElements(...spaceElements);
+const elementsAdded =addClickEventToElements(newGame,...spaceElements);
+setInterval(function(){
+  console.log(newGame.checkWhoIsTheWinner())
+}, 200);
