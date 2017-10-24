9f7a4979c75bd9ac75245768298d53fe3a455b1f
MiguelRamBalt <miguel.ramirez.baltazar@gmail.com>
2017-10-24 16:53:53 -0500

    using loader

---
+++ webpack/newWebpackProject/src/image_viewer.js
@@ -0,0 +1,6 @@
+import '../styles/image_viewer.css';
+
+const  image = document.createElement("img");
+image.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr6ScEgN2QtyXfk8qZVH0CflbimkT9NOSCqQv1b74qxsvXpeey';
+
+document.body.appendChild(image);
