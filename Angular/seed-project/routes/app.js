9080e2339eab742204eb15a8133f8ac9da597782
MiguelRamBalt <miguel.ramirez.baltazar@gmail.com>
2017-11-09 16:14:40 -0600

    Mean basics

--- Angular/seed-project/routes/app.js
+++ Angular/seed-project/routes/app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', { message: req.params.msg });
-});
+router.post('/', function(req, res, next) {
+    console.log('Post message');
 
-router.post('/message', function (req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message );
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Max',
+        lastName: 'Schwarz',
+        password: 'super-secret',
+        email: email
+    });
+    user.save();
+    //res.redirect('/');
 });
 
 module.exports = router;
